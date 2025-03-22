5daf8c64fc9b836f9d2129bde9e5344b2ad15132
Louan Fontenele <louanbastos61@gmail.com>
2025-03-21 22:09:54 -0300

    Update oficinas.php

--- oficinas.php
+++ oficinas.php
@@ -103,7 +103,7 @@ return [
    [
       'id' => 12,
       'descricao' => 'Formação presencial para Equipe Pedagógica da SEMED e Professores do Ensino Regular que atuam nas turmas de Creche ao 5º ano do ensino fundamental com o tema: Avaliação da recomposição da aprendizagem: desafios e perspectivas para garantir a equidade.',
-      'vagas' => 35,
+      'vagas' => 36,
       'areas' => 'Professor(a) de Creche em Tempo Integral|Professora(a) de Creche em Tempo Parcial|Professor(a) Infantil I|Professor(a) Infantil II|Professor(a) de Creche ao 5° Ano Multisseriado|Professores Responsáveis|Professor(a) de 2° do Componente Curricular de Língua Portuguesa e/ou Matemática|Professor(a) de 5° do Componente Curricular de Língua Portuguesa e/ou Matemática|Professor(a) de 9° Ano de Língua Portuguesa|Professor(a) de 9° Ano de Matemática|Professores/APAE|Professor(a) de 1° ao 5° Ano|Professor(a) de 6° ao 9° Ano|Mediador(a) e Facilitador(a) da Educação Integral|Professor(a) e Monitor(a) da Educação Especial e Inclusiva|Gestores Escolares|Supervisores Escolares|Professor(a) da EJA|Equipe Multi Profissionais|Pais de Alunos da Rede Pública Municipal de Creche ao 9° Ano',
       'horas' => '16h (02 dias)',
       'escola' => 'Colégio Militar Tiradentes XXIX', 
